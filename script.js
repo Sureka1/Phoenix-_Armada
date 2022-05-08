90f1303247b8f528e2511562bdecaedc27a3d057
Sureka <sureka0107@gmail.com>
2022-05-08 20:35:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+/*var x = document.getElementById("myBtn");
+x.addEventListener("mouseover", myFunction);
+x.addEventListener("click", mySecondFunction);
+x.addEventListener("mouseout", myThirdFunction);
+
+function myFunction() {
+  document.getElementById("demo").innerHTML += "Moused over!<br>";
+}
+
+function mySecondFunction() {
+  document.getElementById("demo").innerHTML += "Clicked!<br>";
+}
+
+function myThirdFunction() {
+  document.getElementById("demo").innerHTML += "Moused out!<br>";
+}*/
+var Button=document.getElementById('button2').addEventListener('click',
+function buttonClick(){
+    //console.log("button clicked 123");
+    
+});
+
+
+
+
+
+
+
+
+
