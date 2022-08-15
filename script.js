8b61256af768be67fc37a2da91652511599f3018
Sureka <sureka0107@gmail.com>
2022-08-15 16:23:28 +0530

    first commit

--- script.js
+++ script.js
@@ -1,30 +1,14 @@
-/*var x = document.getElementById("myBtn");
-x.addEventListener("mouseover", myFunction);
-x.addEventListener("click", mySecondFunction);
-x.addEventListener("mouseout", myThirdFunction);
-
-function myFunction() {
-  document.getElementById("demo").innerHTML += "Moused over!<br>";
+// aos init //
+AOS.init(); 
+let buttonOne = document.getElementById("color-change-btn-1");
+let buttonTwo = document.getElementById("color-change-btn-2");
+function themeOne(){
+ document.body.style.backgroundColor = "#FFFFFF";
+ document.body.style.color = "#1CD464";
 }
-
-function mySecondFunction() {
-  document.getElementById("demo").innerHTML += "Clicked!<br>";
+function themeTwo(){ 
+    document.body.style.backgroundColor = "#040404";
+    document.body.style.color = "#FCEC34";
 }
-
-function myThirdFunction() {
-  document.getElementById("demo").innerHTML += "Moused out!<br>";
-}*/
-var Button=document.getElementById('button2').addEventListener('click',
-function buttonClick(){
-    //console.log("button clicked 123");
-    
-});
-
-
-
-
-
-
-
-
-
+buttonOne.addEventListener("click", themeOne);
+buttonTwo.addEventListener("click", themeTwo);
